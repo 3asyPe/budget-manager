9e09e4e025c1c79d1def4b5408803e5ee4fe26b2
Harefda <harefda@mail.ru>
2021-07-30 12:57:55 +0300

    urls for static and media, pipenv file

--- server/src/app/settings.py
+++ server/src/app/settings.py
@@ -48,9 +48,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
