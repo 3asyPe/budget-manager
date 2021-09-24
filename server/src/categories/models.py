99ab4293bc6d9a5a189843392eb8b5d4f551b613
Harefda <harefda@mail.ru>
2021-09-24 23:54:57 +0300

    Made classes: Expense, Income, Comission

--- server/src/categories/models.py
+++ server/src/categories/models.py
@@ -7,3 +7,27 @@ class Category(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Expense(Category):
+    expence = models.FloatField(max_length=34)
+
+    class Meta:
+        verbose_name = ("Expense")
+        verbose_name_plural = ("Expenses")
+
+
+class Income(Category):
+    income = models.FloatField(max_length=34)
+
+    class Meta:
+        verbose_name = ("Income")
+        verbose_name_plural = ("Incomes")
+
+
+class Comission(Category):
+    comission = models.FloatField(max_length=34)
+
+    class Meta:
+        verbose_name = ("Comission")
+        verbose_name_plural = ("Comissions")
